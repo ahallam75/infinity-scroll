68da90991b1c903bc9e6a698ff894497928987cc
ahallam <alexhallam75@gmail.com>
2020-07-25 16:40:09 -0400

    API call setup

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hi Alex');
+// Unsplash API
+const count = '10';
+const apiKey = 'INSERT_YOUR_API_KEY_HERE'; 
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Get Photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch error
+  }
+}
+
+// On Load
+getPhotos();
